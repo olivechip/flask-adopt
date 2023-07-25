10e8d4fb2b2cdc52df983d19186958b0b2e50128
Oliver <oliver@MSI>
2023-07-25 14:26:14 -0700

    added sample data for Pets

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from flask-wtf import FlaskForm
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
---
+++ seed.py
@@ -0,0 +1,13 @@
+from models import db, Pet
+from app import app
+
+db.drop_all()
+db.create_all()
+
+p1 = Pet(name='scout', species='cat', age=2)
+p2 = Pet(name='teddy', species='dog', age=6, notes='mini poodle', available=False)
+p3 = Pet(name='bear', species='cat', age=6, notes='persian', available=False)
+p4 = Pet(name='coco', species='cat', age=6, notes='persian', available=False)
+
+db.session.add([p1, p2, p3, p4])
+db.session.commit()
